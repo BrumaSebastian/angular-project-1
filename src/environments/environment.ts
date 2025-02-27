847fe6997694877ef4295bb700da6de4cfbc8a72
Bruma Sebastian <sebastianbruma24@gmail.com>
2025-02-27 20:41:41 +0200

    Fix environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
-  authUri: "http://localhost:9000/application/o/authorize/",
-  tokenUri: "http://localhost:9000/application/o/token/",
+  authUri: 'http://localhost:9000/application/o/authorize/',
+  tokenUri: 'http://localhost:9000/application/o/token/',
   logoutUri:
-    "http://localhost:9000/application/o/dev-authentik-app/end-session/",
-  clientId: "0wCx0kFCBjJ4CKqoNssl1rx85dL183fKJNTqjSSx",
-  clientSecret:
-    "kvV1ylH3DTTJivWb0wrgNURrFehqu9oinX1V3FJAlJYTnKcwRlJFX4ZmnLXF67E3vzKM0GwLhiu4BNZ7T1K3jc0HpVe5zlv1zGSWNkCLabRfxXBMavkIi2mLEmMn1UfE",
-  redirectUri: "http://localhost:4200/callback",
-  dotnetBackEndUrl: "https://localhost:7061/",
-  dotnetBackEndRetrieveTokenEndpoint: "retrieve-token/",
+    'http://localhost:9000/application/o/dev-authentik-app/end-session/',
+  clientId: '',
+  clientSecret: '',
+  redirectUri: 'http://localhost:4200/callback',
+  dotnetBackEndUrl: '',
+  dotnetBackEndRetrieveTokenEndpoint: 'retrieve-token/',
 };
